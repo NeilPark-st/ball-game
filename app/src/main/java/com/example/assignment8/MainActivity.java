3c1f6c23695c834fea40d2136add8ca94c2593e7
Brennan, Gregory <gdb22250@gmx.com>
2019-09-26 20:28:16 +1200

    Release 1 Task 1.1 - full screen display with sticky immersive

---
+++ app/src/main/java/com/example/assignment8/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.assignment8;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Set up full screen display
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        getWindow().getDecorView().setSystemUiVisibility(uiOptions);
+    }
+}
