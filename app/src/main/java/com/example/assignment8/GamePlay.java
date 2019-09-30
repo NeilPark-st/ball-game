a684be74c17f3f9378ae6a2aa22b22caec757ddb
Neil Park <yp73@waikato.ac.nz>
2019-09-30 13:17:01 +1300

    Release 5.2 GameOver & Clear setting

--- app/src/main/java/com/example/assignment8/GamePlay.java
+++ app/src/main/java/com/example/assignment8/GamePlay.java
@@ -6,6 +6,8 @@ import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,15 +18,19 @@ import java.util.ArrayList;
 public class GamePlay extends AppCompatActivity {
 
     int width, height;
-    boolean isEnd, isVic;
+    TextView textViewStage, textViewScore, textViewGameOver, textViewResultScore;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_play);
 
-        isEnd = false;
-        isVic = false;
+        textViewStage = (TextView)findViewById(R.id.textviewStage);
+        textViewScore = (TextView)findViewById(R.id.textviewScore);
+        textViewGameOver = (TextView)findViewById(R.id.textviewGameOver);
+        textViewResultScore = (TextView)findViewById(R.id.textviewResultScore);
+        button = (Button)findViewById(R.id.buttonGoToHome);
 
         //Set up full screen display
         ActionBar actionBar = getSupportActionBar();
@@ -59,6 +65,7 @@ public class GamePlay extends AppCompatActivity {
         float startX, startY, stopX, stopY, length, theta;
         Paint linePaint;
         boolean touchAllowed;
+        boolean isEnd, isVic;
 
         //obstacles
         ArrayList<Obstacle>[] stage;
@@ -66,6 +73,11 @@ public class GamePlay extends AppCompatActivity {
 
         public GraphicsView(Context context) {
             super(context);
+
+            isEnd = false;
+            isVic = false;
+
+
 
             lifeCount = 10;
             score = 0;
@@ -125,136 +137,161 @@ public class GamePlay extends AppCompatActivity {
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
 
-            if(start == true){
-                width = canvas.getWidth();
-                height = canvas.getHeight();
-                playerBall = new PlayerBall(width/2, height - playerBallRadius, playerBallRadius);
-                targetBall = new TargetBall(width/2, targetBallRadius, targetBallRadius);
-                for(int i=0; i < lifeCount; i++){
-                    lifeBalls[i] = new LifeBall(lifeBallRadius * (2* (i % 5) + 1), height - lifeBallRadius * (i / 5 * 2 + 1), lifeBallRadius);
+            if(!isEnd){
+                if(start == true){
+                    width = canvas.getWidth();
+                    height = canvas.getHeight();
+                    playerBall = new PlayerBall(width/2, height - playerBallRadius, playerBallRadius);
+                    targetBall = new TargetBall(width/2, targetBallRadius, targetBallRadius);
+                    for(int i=0; i < lifeCount; i++){
+                        lifeBalls[i] = new LifeBall(lifeBallRadius * (2* (i % 5) + 1), height - lifeBallRadius * (i / 5 * 2 + 1), lifeBallRadius);
+                    }
+                    startX = playerBall.getX();
+                    startY = playerBall.getY();
+                    stopX = startX;
+                    stopY = startY;
+                    length = 150;
+
+                    //Making Obstacles for each stage
+                    //stage2
+                    stage[1].add(new Obstacle(width/4, height/2, obstacleRadius, 0, 0, 0, height ));
+                    stage[1].add(new Obstacle(width/2, height/4, obstacleRadius, 0, 0, 0, height ));
+                    stage[1].add(new Obstacle(width*3/4, height/2, obstacleRadius, 0, 0, 0, height ));
+                    //stage3
+                    stage[2].add(new Obstacle(width * 3 / 8, height / 2, obstacleRadius * 2, 0, 0, 0, height));
+                    stage[2].add(new Obstacle(width / 2, height * 3 / 8, obstacleRadius * 2, 0, 0, 0, height));
+                    stage[2].add(new Obstacle(width * 5 / 8, height / 2, obstacleRadius * 2, 0, 0, 0, height));
+                    //stage4
+                    for(int i=1; i<=1; i++){
+                        stage[3].add(new Obstacle(width * i / 2, height / 4, obstacleRadius, (float)(Math.random() * 5 + 5),
+                                (float)Math.toRadians(180 * i), 0, height));
+                    }
+                    for(int i=1; i<=2; i++){
+                        stage[3].add(new Obstacle(width * i / 3, height / 2, obstacleRadius, (float)(Math.random() * 5 + 5),
+                                (float)Math.toRadians(180 * i), 0, height));
+                    }
+                    for(int i=1; i<=1; i++){
+                        stage[3].add(new Obstacle(width * i / 2, height * 3 / 4, obstacleRadius, (float)(Math.random() * 5 + 5),
+                                (float)Math.toRadians(180 * i), 0, height));
+                    }
+                    //stage5
+                    for(int i=1; i<=1; i++){
+                        stage[4].add(new Obstacle(width * i / 2, height / 4, obstacleRadius, (float)(Math.random() * 5 + 5),
+                                (float)Math.toRadians(Math.random() * 360), targetBallRadius * 6, height - playerBallRadius * 4));
+                    }
+                    for(int i=1; i<=3; i++){
+                        stage[4].add(new Obstacle(width * i / 4, height / 2, obstacleRadius, (float)(Math.random() * 5 + 5),
+                                (float)Math.toRadians(Math.random() * 360), targetBallRadius * 6, height - playerBallRadius * 4));
+                    }
+                    for(int i=1; i<=1; i++){
+                        stage[4].add(new Obstacle(width * i / 2, height * 3 / 4, obstacleRadius, (float)(Math.random() * 5 + 5),
+                                (float)Math.toRadians(Math.random() * 360), targetBallRadius * 6, height - playerBallRadius * 4));
+                    }
+                    //stage[4].add(new Obstacle(width/2, (float)(targetBallRadius * (2 + 1.5)), (int)(targetBallRadius * 1.5), (float)7.5, 0, 0, height));
+
+                    button.setVisibility(INVISIBLE);
+                    start = false;
                 }
-                startX = playerBall.getX();
-                startY = playerBall.getY();
-                stopX = startX;
-                stopY = startY;
-                length = 150;
 
-                //Making Obstacles for each stage
-                //stage2
-                stage[1].add(new Obstacle(width/4, height/2, obstacleRadius, 0, 0, 0, height ));
-                stage[1].add(new Obstacle(width/2, height/4, obstacleRadius, 0, 0, 0, height ));
-                stage[1].add(new Obstacle(width*3/4, height/2, obstacleRadius, 0, 0, 0, height ));
-                //stage3
-                stage[2].add(new Obstacle(width * 3 / 8, height / 2, obstacleRadius * 2, 0, 0, 0, height));
-                stage[2].add(new Obstacle(width / 2, height * 3 / 8, obstacleRadius * 2, 0, 0, 0, height));
-                stage[2].add(new Obstacle(width * 5 / 8, height / 2, obstacleRadius * 2, 0, 0, 0, height));
-                //stage4
-                for(int i=1; i<=1; i++){
-                    stage[3].add(new Obstacle(width * i / 2, height / 4, obstacleRadius, (float)(Math.random() * 5 + 5),
-                            (float)Math.toRadians(180 * i), 0, height));
+                textViewStage.setText("Stage: " + currentStage);
+                textViewScore.setText("Score: " + score);
+
+                if(lifeCount <= 0){
+                    isEnd = true;
+                    if(score >= 10)
+                        isVic = true;
                 }
-                for(int i=1; i<=2; i++){
-                    stage[3].add(new Obstacle(width * i / 3, height / 2, obstacleRadius, (float)(Math.random() * 5 + 5),
-                            (float)Math.toRadians(180 * i), 0, height));
+
+                switch (score){
+                    case 0:
+                        currentStage = 1;
+                        break;
+                    case 1:
+                    case 2:
+                        currentStage = 2;
+                        break;
+                    case 3:
+                    case 4:
+                        currentStage = 3;
+                        break;
+                    case 5:
+                    case 6:
+                    case 7:
+                        currentStage = 4;
+                        break;
+                    case 8:
+                    case 9:
+                        currentStage = 5;
+                        break;
                 }
-                for(int i=1; i<=1; i++){
-                    stage[3].add(new Obstacle(width * i / 2, height * 3 / 4, obstacleRadius, (float)(Math.random() * 5 + 5),
-                            (float)Math.toRadians(180 * i), 0, height));
+
+                canvas.drawCircle(playerBall.getX(), playerBall.getY(), playerBall.getRadius(), playerBall.getPaint());
+                canvas.drawCircle(targetBall.getX(), targetBall.getY(), targetBall.getRadius(), targetBall.getPaint());
+                for(int i = 0; i < lifeCount - 1; i++){
+                    canvas.drawCircle(lifeBalls[i].getX(), lifeBalls[i].getY(), lifeBalls[i].getRadius(), lifeBalls[i].getPaint());
                 }
-                //stage5
-                for(int i=1; i<=1; i++){
-                    stage[4].add(new Obstacle(width * i / 2, height / 4, obstacleRadius, (float)(Math.random() * 5 + 5),
-                            (float)Math.toRadians(Math.random() * 360), targetBallRadius * 6, height - playerBallRadius * 4));
+                playerBall.changePosition();
+                targetBall.changePosition();
+
+                canvas.drawLine(startX, startY, stopX, stopY, linePaint);
+
+                //when the ball is out from the view
+                if(playerBall.getY() + playerBall.getRadius() < 0){
+                    lifeCount--;
+                    playerBall.setPositionToInitial();
+                    playerBall.setVel(0,0);
+                    touchAllowed = true;
                 }
-                for(int i=1; i<=3; i++){
-                    stage[4].add(new Obstacle(width * i / 4, height / 2, obstacleRadius, (float)(Math.random() * 5 + 5),
-                            (float)Math.toRadians(Math.random() * 360), targetBallRadius * 6, height - playerBallRadius * 4));
+
+                //drawing obstacles for the right stage
+                currentStageObstacle = stage[currentStage - 1];
+                for(int i = 0; i < currentStageObstacle.size(); i++){
+                    canvas.drawCircle(currentStageObstacle.get(i).getX(), currentStageObstacle.get(i).getY(),
+                            currentStageObstacle.get(i).getRadius(), currentStageObstacle.get(i).getPaint());
+                    currentStageObstacle.get(i).changePosition();
                 }
-                for(int i=1; i<=1; i++){
-                    stage[4].add(new Obstacle(width * i / 2, height * 3 / 4, obstacleRadius, (float)(Math.random() * 5 + 5),
-                            (float)Math.toRadians(Math.random() * 360), targetBallRadius * 6, height - playerBallRadius * 4));
-                }
-                //stage[4].add(new Obstacle(width/2, (float)(targetBallRadius * (2 + 1.5)), (int)(targetBallRadius * 1.5), (float)7.5, 0, 0, height));
 
-                start = false;
-            }
-
-            if(lifeCount <= 0){
-                isEnd = true;
-                if(score >= 10)
-                    isVic = true;
-            }
-
-            switch (score){
-                case 0:
-                    currentStage = 1;
-                    break;
-                case 1:
-                case 2:
-                    currentStage = 2;
-                    break;
-                case 3:
-                case 4:
-                    currentStage = 3;
-                    break;
-                case 5:
-                case 6:
-                case 7:
-                    currentStage = 4;
-                    break;
-                case 8:
-                case 9:
-                    currentStage = 5;
-                    break;
-            }
-
-            canvas.drawCircle(playerBall.getX(), playerBall.getY(), playerBall.getRadius(), playerBall.getPaint());
-            canvas.drawCircle(targetBall.getX(), targetBall.getY(), targetBall.getRadius(), targetBall.getPaint());
-            for(int i = 0; i < lifeCount - 1; i++){
-                canvas.drawCircle(lifeBalls[i].getX(), lifeBalls[i].getY(), lifeBalls[i].getRadius(), lifeBalls[i].getPaint());
-            }
-            playerBall.changePosition();
-            targetBall.changePosition();
-
-            canvas.drawLine(startX, startY, stopX, stopY, linePaint);
-
-            //when the ball is out from the view
-            if(playerBall.getY() + playerBall.getRadius() < 0){
-                lifeCount--;
-                playerBall.setPositionToInitial();
-                playerBall.setVel(0,0);
-                touchAllowed = true;
-            }
-
-            //drawing obstacles for the right stage
-            currentStageObstacle = stage[currentStage - 1];
-            for(int i = 0; i < currentStageObstacle.size(); i++){
-                canvas.drawCircle(currentStageObstacle.get(i).getX(), currentStageObstacle.get(i).getY(),
-                        currentStageObstacle.get(i).getRadius(), currentStageObstacle.get(i).getPaint());
-                currentStageObstacle.get(i).changePosition();
-            }
-
-            //if the player ball collides with the target
-            if(playerBall.isCollideWith(targetBall)){
-                playerBall.setPositionToInitial();
-                playerBall.setVel(0,0);
-                lifeCount--;
-                score++;
-                touchAllowed = true;
-            }
-
-            //if the player ball collides with obstacles
-            for(int i=0; i<currentStageObstacle.size(); i++){
-                if(playerBall.isCollideWith(currentStageObstacle.get(i))){
+                //if the player ball collides with the target
+                if(playerBall.isCollideWith(targetBall)){
                     playerBall.setPositionToInitial();
                     playerBall.setVel(0,0);
                     lifeCount--;
-                    currentStageObstacle.remove(i);
+                    score++;
                     touchAllowed = true;
                 }
+
+                //if the player ball collides with obstacles
+                for(int i=0; i<currentStageObstacle.size(); i++){
+                    if(playerBall.isCollideWith(currentStageObstacle.get(i))){
+                        playerBall.setPositionToInitial();
+                        playerBall.setVel(0,0);
+                        lifeCount--;
+                        currentStageObstacle.remove(i);
+                        touchAllowed = true;
+                    }
+                }
+
+                invalidate();
+            }
+            else{ // Game Over
+
+                if(isVic){ // Victory
+                    textViewGameOver.setTextColor(getColor(R.color.colorClear));
+                    textViewGameOver.setText("Clear!!");
+                }
+                else{
+                    textViewGameOver.setTextColor(getColor(R.color.colorGameOver));
+                    textViewGameOver.setText("Game Over");
+                }
+
+                textViewResultScore.setText("Score: " + score);
+                button.setVisibility(VISIBLE);
+                textViewStage.setText("");
+                textViewScore.setText("");
             }
 
-            invalidate();
+
+
         }
 
 
